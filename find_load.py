f718c65d52f7e4eaecd7bf0450a67bada67da717
Ergo Adams <ergoa2001@gmail.com>
2022-01-15 18:06:56 +0200

    Fix find_load.py not displaying all calls

--- find_load.py
+++ find_load.py
@@ -13,15 +13,15 @@ dest_address = int("0x" + sys.argv[1], base=16)
 LOAD_OP_START = (0x9F << 40) | (0x80 << 32)
 full_load = LOAD_OP_START + dest_address
 
-found_address = False
+
+total_count = 0
 for reg in range(16):
     in_bytes = (full_load + (reg << 32)).to_bytes(6, 'big')
     if in_bytes in data:
-        print("!!Found {} being loaded into register {} at address {}".format(sys.argv[1], reg, hex(data.index(in_bytes) + 0x10000000)))
-        found_address = True
-if not found_address:
-    print("Address not found in firmware as a jump to register")
-    print("Trying to look for offset calls")
+        res = [i for i in range(len(data)) if data.startswith(in_bytes, i)]
+        for address in res:
+            print("!!Found {} being loaded into register {} at address {}".format(sys.argv[1], reg, hex(address + 0x10000000)))
+            total_count += 1
 
 CALL_D_OP_START =   0b1101100000000000
 CALL_OP_START =     0b1101000000000000
@@ -32,7 +32,11 @@ for i in range(len(data)//2):
     if cur_opcode & CALL_D_OP_START == CALL_D_OP_START:
         if dest_address == binary_offset + 0x10000000 + ((ctypes.c_int32(cur_opcode & 0b11111111111).value << 22) >> 21) + 2:
             print("!!Found call:d at {}".format(hex(binary_offset + 0x10000000)))
+            total_count += 1
     elif cur_opcode & CALL_OP_START == CALL_OP_START:
         if dest_address == binary_offset + 0x10000000 + ((ctypes.c_int32(cur_opcode & 0b11111111111).value << 22) >> 21):
             print("!!Found call at {}".format(hex(binary_offset + 0x10000000)))
+            total_count += 1
+
+print("Found a total of {} calls".format(total_count))
 
